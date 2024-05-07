39a742137ea5bdab6f2c6e7cbaaeee31e1e42704
Hugo EXTRAT <extrat.h@gmail.com>
2024-05-07 20:16:52 +0200

    fix: add AuthenticationServices

--- ios/RCTBraintree/RCTBraintree.h
+++ ios/RCTBraintree/RCTBraintree.h
@@ -10,6 +10,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTUtils.h>
 #import <React/RCTConvert.h>
+#import <AuthenticationServices/AuthenticationServices.h>
 
 #import "Braintree/Braintree-Swift.h"
 
